59ede59a5f3243300831c57ed9e2a50465dc5b0f
Tugdual Sarazin <tugdual.sarazin@gmail.com>
2025-05-07 11:36:16 +0200

    Fix minor React imports

--- frontend/src/pages/SourceRoundupPage.tsx
+++ frontend/src/pages/SourceRoundupPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { generatePrompt, findSources, fetchProjectById } from "../api";
 import SourceCard from "../components/SourceCard";
